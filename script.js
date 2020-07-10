6b3f320eb8adf85c1e664e31465885c78befb03f
Lewis <mr.robot@Lewiss-MBP.attlocal.net>
2020-07-09 20:58:07 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let two=2;
+let oneFourth='1/4';
+function marinaraSauce(cookName){
+    console.log('Hey '+ cookName+'! This is my marinara sauce recipe!')
+    console.log('heat a medium-large saucepann over medium heat.');
+    console.log('add '+two  +' tsp of oil, 5 garlic cloves, cook until golden, about '+two+' minutes.');
+    console.log('add '+oneFourth+ ' cup of water, '+two +' cans of crushed tomatoes, salt and season with black pepper to taste');
+    console.log('cover the pot and bring to boil, reduce the heat to medium low and simmer and simmer until the sauce is heated, about 10 minutes');
+    console.log('stir in '+oneFourth +' cup roughly chopped basil, salt and pepper as needed');
+}
+marinaraSauce('Adam');
